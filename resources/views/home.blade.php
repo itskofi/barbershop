f4388209514838d1bcc8fa4257bab8b5ae3f78aa
Kofi <kofi_offei@icloud.com>
2023-08-30 17:51:59 +0200

    feat: added images and hero section plus cover image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Barber</title>
+  @vite('resources/css/app.css')
 </head>
 <body>
-  
+
+  <div class="bg-fixed">
+      <img class="object-cover w-full h-full " src="{{ url('images/barber-chair-2.jpg') }}" alt="">
+  </div>
+
+  <div class="flex bg-gray-100">
+    <div class="w-1/2 my-auto px-2 text-xs sm:text-xl space-y-2">
+      <div class="text-xl sm:text-4xl font-semibold">Barbershop Team</div>
+      <div class="font-bold uppercase">Your local Family</div>
+      <div class="">We are here to give you the best haircut you can imagine</div>
+    </div>
+    <div class="w-1/2">
+      <img src="{{ url('images/barber-team.jpg') }}" alt="">
+    </div>
+  </div>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./resources/**/*.{html,js}"],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
   theme: {
     extend: {
       colors: {
