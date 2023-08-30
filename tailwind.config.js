2c4edc68192b44b728b6d9741664cf50689806f1
Kofi <kofi_offei@icloud.com>
2023-08-30 15:45:16 +0200

    feat: install alpinejs, tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./resources/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#9b7c64',
+        'secondary': '#f1d7b4',
+      }
+    },
+  },
+  plugins: [],
+}
+
