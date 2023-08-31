6ed4a68a0415cac6492fe2e6539242be159cc867
Kofi <kofi_offei@icloud.com>
2023-08-31 15:20:17 +0200

    feat: adjust cover text

--- resources/views/components/cover.blade.php
+++ resources/views/components/cover.blade.php
@@ -1,6 +1,9 @@
 <div class="relative bg-fixed h-64 sm:h-72 md:h-96">
-  <img class="object-cover w-full h-full " src="{{ url('images/barber-chair-2.jpg') }}" alt="">
-  <div class="absolute top-1/4 right-1/3">
-      <div class="inline text-white shadow-xl text-2xl sm:text-3xl">Gentlemens Barber</div>
+  <div class="absolute top-1.5 left-3">
+      <div class="inline text-primary text-2xl sm:text-3xl font-thin">Gentlemens Barber</div>
   </div>
+  <div class="absolute top-6 top left-6">
+      <div class="inline text-secondary text-2xl sm:text-3xl">Gentlemens Barber</div>
+  </div>
+  <img class="object-cover w-full h-full " src="{{ url('images/barber-chair-2.jpg') }}" alt="">
 </div>
