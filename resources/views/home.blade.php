604ff4cbd94fbbb8439dbb014fef32a67e5e225a
Kofi <kofi_offei@icloud.com>
2023-09-01 01:12:15 +0200

    feat: add navigation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Barber</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="max-w-5xl mx-auto">
+    <x-navigation />
     <x-cover />
     <x-hero />
     <x-hero2 />
