29881cb5c98e1f917ef5481801c213111a222064
Kofi <kofi_offei@icloud.com>
2023-08-31 09:51:46 +0200

    feat: adjust padding and set team member images h-centered

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,17 +8,17 @@
 </head>
 <body>
 
-  <div class="relative bg-fixed h-64">
+  <div class="relative bg-fixed h-64 sm:h-72">
       <img class="object-cover w-full h-full " src="{{ url('images/barber-chair-2.jpg') }}" alt="">
       <div class="absolute top-1/4 right-1/3">
-        <div class="inline text-white shadow-xl text-2xl">Gentlemens Barber</div>
+        <div class="inline text-white shadow-xl text-2xl sm:text-3xl">Gentlemens Barber</div>
       </div>
   </div>
 
-  <div class="mt-4 py-6 px-2 flex">
-    <div class="px-2 w-1/2 my-auto text-xs sm:text-xl">
+  <div class="py-6 px-2 flex">
+    <div class="px-2 w-1/2 my-auto text-xs sm:text-base sm:px-6 sm:flex sm:flex-col sm:space-y-1">
       <div class="text-primary font-bold uppercase">Your local Family</div>
-      <div class="text-xl sm:text-4xl font-semibold">We are</div>
+      <div class="text-xl sm:text-2xl font-semibold">We are</div>
       <div class="mt-2">We are here to give you the best haircut you can imagine</div>
     </div>
     <div class="w-1/2">
@@ -26,10 +26,10 @@
     </div>
   </div>
 
-  <div class="mt-4 py-6 px-2 flex flex-row-reverse ">
-    <div class="w-1/2 my-auto px-2 text-xs sm:text-xl">
+  <div class="py-6 px-2 flex flex-row-reverse ">
+    <div class="w-1/2 my-auto px-2 text-xs sm:text-base sm:px-6 sm:flex sm:flex-col sm:space-y-1">
       <div class="text-primary font-bold uppercase tracking">Renew your look</div>
-      <div class="text-xl sm:text-4xl font-semibold">You get the best</div>
+      <div class="text-xl sm:text-2xl font-semibold">You get the best</div>
       <div class="mt-2">With over a decade experience you will get the best experience</div>
     </div>
     <div class="w-1/2">
@@ -37,21 +37,21 @@
     </div>
   </div>
 
-  <div class="mt-4 py-6">
-    <div class="text-xl font-semibold text-center">The Team</div>
-    <div class="mt-4 flex">
+  <div class="py-6">
+    <div class="text-xl font-semibold text-center sm:text-2xl">The Team</div>
+    <div class="flex sm:pt-4">
       <div class="px-2 flex flex-col w-1/3">
-        <div class="">
+        <div class="sm:mx-auto">
           <img class="rounded-lg object-cover h-48" src="{{ url('images/barber-1.jpg') }}" alt="">
         </div>
-        <div class="text-center ">
+        <div class="text-center">
           <div class="text-base">John Elfa</div>
           <div class="text-gray-400 text-xs">Specialized in Mens Hair</div>
         </div>
       </div>
 
       <div class="px-2 flex flex-col w-1/3">
-        <div class="">
+        <div class="sm:mx-auto">
           <img class="rounded-lg object-cover h-48" src="{{ url('images/barber-2.webp') }}" alt="">
         </div>
         <div class="text-center ">
@@ -61,7 +61,7 @@
       </div>
 
       <div class="px-2 flex flex-col w-1/3">
-        <div class="">
+        <div class="sm:mx-auto">
           <img class="rounded-lg object-cover h-48" src="{{ url('images/barber-3.jpg') }}" alt="">
         </div>
         <div class="text-center">
@@ -72,9 +72,9 @@
     </div>
   </div>
 
-  <div class="mt-4 py-6 px-2">
-    <div class="text-xl font-semibold text-center">Services</div>
-    <div class="flex flex-col divide-dotted divide-y rounded-lg shadow-lg bg-primary text-white p-4">
+  <div class="py-6 px-2">
+    <div class="text-xl font-semibold text-center sm:pb-4">Services</div>
+    <div class="flex flex-col divide-dotted divide-y rounded-lg shadow-lg bg-primary text-white p-4 sm:px-20">
       <div class="flex justify-between">
         <div class="">Haircut</div>
         <div class="text-secondary font-semibold">25€</div>
@@ -96,35 +96,35 @@
         <div class="text-secondary font-semibold">35€</div>
       </div>
     </div>
-    <div class="flex w-full justify-center mt-2">
+    <div class="flex w-full justify-center sm:pt-4 ">
       <button class="px-2 rounded-lg font-semibold uppercase text-black text-sm bg-secondary">Book an appointment</button>
     </div>
   </div>
 
-  <div class="mt-4 py-6 px-2">
+  <div class="py-6 px-2">
     <div class="">
       <div class="text-xl font-semibold text-center">Looks</div>
     </div>
-    <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="{{ url('images/haircut-1.jpg')}}" alt="">
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-2 sm:pt-4">
+      <div class="mx-auto">
+          <img class="w-64 rounded-lg" src="{{ url('images/haircut-1.jpg')}}" alt="">
       </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="{{ url('images/haircut-2.jpg')}}" alt="">
+      <div class="mx-auto">
+          <img class="w-64 rounded-lg" src="{{ url('images/haircut-2.jpg')}}" alt="">
       </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="{{ url('images/haircut-3.jpg')}}" alt="">
+      <div class="mx-auto">
+          <img class="w-64 rounded-lg" src="{{ url('images/haircut-3.jpg')}}" alt="">
       </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="{{ url('images/haircut-4.jpg')}}" alt="">
+      <div class="mx-auto">
+          <img class="w-64 rounded-lg" src="{{ url('images/haircut-4.jpg')}}" alt="">
       </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="{{ url('images/haircut-6.webp')}}" alt="">
+      <div class="mx-auto">
+          <img class="w-64 rounded-lg" src="{{ url('images/haircut-6.webp')}}" alt="">
       </div>
     </div>
   </div>
 
-  <div class="mt-4 py-6 px-2 text-center">
+  <div class="py-6 px-2 text-center">
     <div class="text-base">
       <div class="text-xl font-semibold">Find our Barbershop</div>
       <div class="mt-2">
